31d36a540d0d1775f311c6fa16835f17b3a742a4
Prince kathiriya <princekathiriya62@gmail.com>
2023-08-09 19:01:35 +0530

    cpp

--- restroproject.cpp
+++ restroproject.cpp
@@ -12,7 +12,7 @@ public:
     {
         cout << "Enter Customer name :" << endl;
         cin >> name;
-        cout << " Mobile Number :" << endl;
+        cout << "Mobile Number :" << endl;
         cin >> number;
     }
 };
@@ -28,109 +28,131 @@ int calc(int x)
     cgst = (x * 2.5) / 100;
     bill = x - discount + sgst + cgst;
 
-    cout << "<<<<<<<<<welcome to 3story.>>>>>>>>>" << endl;
+    cout << "\t\t=======================================" << endl;
+    
+    cout << "\t\t=======================================" << endl;
 
-    cout << " Discount=>  " << discount << endl;
-    cout << " SGST=>" << sgst << endl;
-    cout << " CGST=>" << cgst << endl;
-    cout << " Net amount=>" << bill << endl;
+    cout << "\t\t Total bill=" << x << endl;
+    cout << "\t\t Discount=" << discount << endl;
+    cout << "\t\t SGST=" << sgst << endl;
+    cout << "\t\t CGST=" << cgst << endl;
+    cout << "\t\t payable amount=" << bill << endl;
+    cout << "\t\t=======================================" << endl;
+    cout << "\t\t        Thank you visit again." << endl;
+    cout << "\t\t=======================================" << endl;
 }
 
 int main()
 {
     cafe b;
     int ch;
-    int quantity;
+    int qty[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int price;
     int amount;
     int total = 0;
 
+    cout << "\t\t=======================================" << endl;
+    cout << "\t\t\t   welcome to 3story." << endl;
+    cout << "\t\t=======================================" << endl;
+    cout << "\t\t1.cappacino...........................120 Rs" << endl;
+    cout << "\t\t2.frappe..............................130 Rs" << endl;
+    cout << "\t\t3.chocalate frappe....................130 Rs" << endl;
+    cout << "\t\t4.hot chocalate.......................140 Rs" << endl;
+    cout << "\t\t5.garlic rice.........................130 Rs" << endl;
+    cout << "\t\t6.mocktail............................120 Rs" << endl;
+    cout << "\t\t7.mint mohito.........................130 Rs" << endl;
+    cout << "\t\t8.ice cream...........................130 Rs" << endl;
+    cout << "\t\t9.coco................................150 Rs" << endl;
+    cout << "\t\t10. choco thickshake..................110 Rs" << endl;
+    cout << "\t\t=======================================" << endl;
+    cout << "\t\t\tConfirm Order press 11" << endl;
+    cout << "\t\t=======================================" << endl;
     b.info();
-
     do
     {
-
-        cout << "<<<<<<<<<welcome to 3story.>>>>>>>>>" << endl;
-        cout << "1.cappacino..........120 Rs                  6.mocktail............120 Rs " << endl;
-        cout << "2.frappe...........130 Rs                  7.mint mohito......130 Rs " << endl;
-        cout << "3.chocalate frappe.....130 Rs                  8.ice cream.........130 Rs " << endl;
-        cout << "4.hot chocalate..............140 Rs                 9.coco..............150 Rs" << endl;
-        cout << "5.garlic rice..............130 Rs                 10. choco thickshake.......110 Rs" << endl;
-        cout << "11.bill plz                           " << endl;
 
         cout << "Enter your order: " << endl;
         cin >> ch;
         switch (ch)
         {
         case 1:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = 120 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[0];
+            amount = 120 * qty[0];
             total += amount;
+            qty[0] += qty[0];
             break;
 
         case 2:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 130 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[1];
+            amount =  130 * qty[1];
             total += amount;
+            qty[1] += qty[1];
             break;
 
         case 3:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 130 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[2];
+            amount = 130 * qty[2];
             total += amount;
+            qty[2] += qty[2];
             break;
 
         case 4:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-
-            amount = total + 140 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[3];
+            amount = 140 * qty[3];
             total += amount;
+            qty[3] += qty[3];
             break;
 
         case 5:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 130 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[4];
+            amount = 130 * qty[4];
             total += amount;
+            qty[4] += qty[4];
             break;
 
         case 6:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 120 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[5];
+            amount = 120 * qty[5];
             total += amount;
+            qty[5] += qty[5];
             break;
 
         case 7:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 130 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[6];
+            amount = 130 * qty[6];
             total += amount;
+            qty[6] += qty[6];
             break;
 
         case 8:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 130 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[7];
+            amount = 130 * qty[7];
             total += amount;
+            qty[7] += qty[7];
             break;
 
         case 9:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 150 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[8];
+            amount = 150 * qty[8];
             total += amount;
+            qty[8] += qty[8];
             break;
 
         case 10:
-            cout << "Enter quantity: " << endl;
-            cin >> quantity;
-            amount = total + 110 * quantity;
+            cout << "Enter qty: " << endl;
+            cin >> qty[9];
+            amount = 110 * qty[9];
             total += amount;
+            qty[9] += qty[9];
             break;
 
         case 11:
@@ -144,6 +166,67 @@ int main()
         }
 
     } while (ch != 11);
+
+    cout << "\t\t=======================================" << endl;
+    cout << "\t\t<<<<<<<<<welcome to 3story.>>>>>>>>>" << endl;
+    cout << "\t\t=======================================" << endl;
+    cout << "\t\tItem"
+         << "\t"
+         << "Qty"
+         << "\t"
+         << "Price"
+         << "\t"
+         << "total" << endl;
+    if (qty[0] != 0)
+        cout << "\t\t1.cappacino"
+             << "\t" << qty[0] << "\t"
+             << "120"
+             << "\t" << 120 * qty[0] << endl;
+    if (qty[1] != 0)
+        cout << "\t\t2.frappe"
+             << "\t" << qty[1] << "\t"
+             << "130"
+             << "\t" << 130 * qty[1] << endl;
+    if (qty[2] != 0)
+        cout << "\t\t3.chocalate frappe"
+             << "\t" << qty[2] << "\t"
+             << "120"
+             << "\t" << 120 * qty[2] << endl;
+    if (qty[3] != 0)
+        cout << "\t\t4.hot chocalate"
+             << "\t" << qty[3] << "\t"
+             << "140"
+             << "\t" << 140 * qty[3] << endl;
+    if (qty[4] != 0)
+        cout << "\t\t5.garlic rice"
+             << "\t" << qty[4] << "\t"
+             << "130"
+             << "\t" << 130 * qty[4] << endl;
+    if (qty[5] != 0)
+        cout << "\t\t6.mocktail"
+             << "\t" << qty[5] << "\t"
+             << "120 "
+             << "\t" << 120 * qty[5] << endl;
+    if (qty[6] != 0)
+        cout << "\t\t7.mint mohito"
+             << "\t" << qty[6] << "\t"
+             << "130"
+             << "\t" << 130 * qty[6] << endl;
+    if (qty[7] != 0)
+        cout << "\t\t8.ice cream"
+             << "\t" << qty[7] << "\t"
+             << "130"
+             << "\t" << 130 * qty[7] << endl;
+    if (qty[8] != 0)
+        cout << "\t\t9.coco"
+             << "\t" << qty[8] << "\t"
+             << "150"
+             << "\t" << 150 * qty[8] << endl;
+    if (qty[9] != 0)
+        cout << "\t\t10.choco thickshake"
+             << "\t" << qty[9] << "\t"
+             << "110"
+             << "\t" << 110 * qty[9] << endl;
 
     calc(total);
 }
